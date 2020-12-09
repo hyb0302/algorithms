af8b9de92c9a02c036c40ae906291da05831bd29
hyb <251420906@qq.com>
2020-12-09 19:36:30 +0800

    字符串匹配算法 RK 实现

--- src/main/java/stringmatching/RK.java
+++ src/main/java/stringmatching/RK.java
@@ -3,13 +3,52 @@ package stringmatching;
 /**
  * @author huangyb
  * @date 2020/12/6
+ * Rabin-Karp
  */
 public class RK implements StringMatching {
 
     @Override
     public boolean contains(String main, String pattern) {
+        char[] mainChars = main.toCharArray();
+        char[] patternChars = pattern.toCharArray();
+        if (mainChars.length < patternChars.length) {
+            return false;
+        }
+        int[] mainHash = new int[mainChars.length - patternChars.length + 1];
+        //计算主串的哈希值
+        for (int i = 0; i < mainChars.length - patternChars.length + 1; i++) {
+            int hash = 0;
+            for (int j = 0; j < patternChars.length; j++) {
+                hash += getNum(mainChars[i + j]);
+            }
+            mainHash[i] = hash;
+        }
+        int patternHash =  0;
+        for (int i = 0; i < patternChars.length; i++) {
+            patternHash += getNum(patternChars[i]);
+        }
 
-
+        for (int i = 0; i < mainHash.length; i++) {
+            if (mainHash[i] == patternHash) {
+                boolean f = true;
+                for (int j = 0; j < patternChars.length; j++) {
+                    if (mainChars[i + j] != patternChars[j]) {
+                        f = false;
+                    }
+                }
+                if (f) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+
+    public int getNum(char c) {
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Test.test(new RK());
     }
 }
--- src/main/java/stringmatching/Test.java
+++ src/main/java/stringmatching/Test.java
@@ -1,11 +1,22 @@
 package stringmatching;
 
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author huangyb
  * @date 2020/12/8
  */
 public class Test {
+
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
 
     private static void t1(StringMatching s) {
         assert s.contains("s", "s");
@@ -18,12 +29,76 @@ public class Test {
         assert s.contains("hello", "ll");
         assert !s.contains("hello", "ab");
         assert s.contains("aaba", "ab");
+        assert !s.contains("asba", "ab");
     }
 
     public static void test(StringMatching s) {
         t1(s);
-        System.out.println("ok");
+//        System.out.println("ok");
     }
 
+    public static void randomTest(StringMatching s) {
+        s.contains(randomString(10), randomString(8));
+    }
+
+    private static String randomString(int minLength, int maxLength) {
+        return randomString(minLength + RANDOM.nextInt(maxLength - minLength));
+    }
+
+    private static String randomString(int length) {
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++) {
+            chars[i] = (char) (97 + RANDOM.nextInt(26));
+        }
+        return new String(chars);
+    }
+
+    private static void testInTheSameData(int times) {
+        List<List<String>> l = new ArrayList<>(times);
+        for (int i = 0; i < times; i++) {
+            l.add(Arrays.asList(randomString(10), randomString(3, 6)));
+        }
+        RK rk = new RK();
+        BF bf = new BF();
+        randomTest(bf);
+        randomTest(rk);
+
+
+
+        LocalDateTime start1 = LocalDateTime.now();
+        for (int i = 0; i < times; i++) {
+            bf.contains(l.get(i).get(0), l.get(i).get(1));
+        }
+        System.out.println("bf: " + Duration.between(start1, LocalDateTime.now()).getNano());
+
+        LocalDateTime start = LocalDateTime.now();
+        for (int i = 0; i < times; i++) {
+            rk.contains(l.get(i).get(0), l.get(i).get(1));
+        }
+        System.out.println("rk: " + Duration.between(start, LocalDateTime.now()).getNano());
+
+    }
+
+    public static void main(String[] args) {
+//        int times = 10000;
+//        RK rk = new RK();
+//        BF bf = new BF();
+//        randomTest(bf);
+//        randomTest(rk);
+//
+//        LocalDateTime start = LocalDateTime.now();
+//        for (int i = 0; i < times; i++) {
+//            randomTest(rk);
+//        }
+//        System.out.println("rk: " + Duration.between(start, LocalDateTime.now()).getNano());
+//
+//        LocalDateTime start1 = LocalDateTime.now();
+//        for (int i = 0; i < times; i++) {
+//            randomTest(bf);
+//        }
+//        System.out.println("bf: " + Duration.between(start1, LocalDateTime.now()).getNano());
+
+        testInTheSameData(10000);
+    }
 
 }
